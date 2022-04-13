5ef492ba611a0fde5e9b92e8414e2380cc3bc284
KhoiNT <khoinguyen@chotot.vn>
2022-04-14 04:13:48 +0700

    add test grpc client

--- pkg/test_client/test_client.go
+++ pkg/test_client/test_client.go
@@ -1,1 +1,20 @@
-package test_client
+package main
+
+import (
+	"context"
+	"fmt"
+	svr "github.com/ct-core-standard/pkg/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, _ := grpc.Dial("127.0.0.1:8989", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	client := svr.NewAdListingServiceClient(conn)
+	req := &svr.GetAdInfoRequest{ListId: 102993}
+	res, err := client.GetAdInfo(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
+}
